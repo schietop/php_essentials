fab1b4898af1bdf38793763132b5c0dd955c8c13
schietop <schietop@hotmail.com>
2015-09-28 13:41:04 +0200

    hello world

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8" />
+	<title>Document title</title>
+	</head>
+	<body>
+		<?php echo "Hello World!"; ?><br/>
+		<?php echo "Hello" . " World!"; ?><br/>
+		<?php echo 2+3; ?>
+	</body>
+</html>
